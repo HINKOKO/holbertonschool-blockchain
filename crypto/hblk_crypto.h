8677f055f73a8e0b308f33e622f315bf1eadec64
HINKOKO <piotr62@live.fr>
2023-10-23 15:27:40 +0200

    Betty-lizing the header file

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,14 +1,13 @@
 #ifndef __HBTN_CRYPTO__
 #define __HBTN_CRYPTO__
 
-
 #include <openssl/sha.h>
 /**
  * Using stdint.h typedefs to declare most of our basic data types
  * => to maintain same sizes depending on the system architecture
  * our code will be executed on. Always the same sizes, no matter
  * the architecture
-*/
+ */
 #include <stdint.h>
 
 #define EC_CURVE NID_secp256k1
@@ -23,26 +22,23 @@
 
 /**
  * struct sig_s - EC Signature structure
- * 
  * @sig: Signature buffer. The whole spae may not be used
  * @len: Actual signature size. Can't exceed SIG_MAX_LEN, therefore stored
  * on a byte
-*/
+ */
 
 typedef struct sig_s
 {
 	/**
 	 * @sig must stay first, so we can directly use the
 	 * structure as an array of char
-	*/
+	 */
 	uint8_t sig[SIG_MAX_LEN];
 	uint8_t len;
 } sig_t;
 
 /* Protozo-aires */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
-
-
-
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH]);
 
 #endif /* __HBTN_CRYPTO__ */
