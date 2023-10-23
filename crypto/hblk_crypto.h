ff1fe078862c23310bdddc814f6e0a607af1835d
HINKOKO <piotr62@live.fr>
2023-10-23 15:31:11 +0200

    What the heack Betty you are angry !

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,14 +1,9 @@
 #ifndef __HBTN_CRYPTO__
 #define __HBTN_CRYPTO__
 
-#include <openssl/sha.h>
-/**
- * Using stdint.h typedefs to declare most of our basic data types
- * => to maintain same sizes depending on the system architecture
- * our code will be executed on. Always the same sizes, no matter
- * the architecture
- */
 #include <stdint.h>
+/* stdint types will maintain size regardless architecture */
+#include <openssl/sha.h>
 
 #define EC_CURVE NID_secp256k1
 
@@ -23,16 +18,14 @@
 /**
  * struct sig_s - EC Signature structure
  * @sig: Signature buffer. The whole spae may not be used
+ * => sig must stay first, so we can directly use the
+ * structure as an array of char
  * @len: Actual signature size. Can't exceed SIG_MAX_LEN, therefore stored
  * on a byte
  */
 
 typedef struct sig_s
 {
-	/**
-	 * @sig must stay first, so we can directly use the
-	 * structure as an array of char
-	 */
 	uint8_t sig[SIG_MAX_LEN];
 	uint8_t len;
 } sig_t;
