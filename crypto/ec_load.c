accdb2738260ff464750ba43daaa9d1e11d1b0c8
HINKOKO <piotr62@live.fr>
2023-10-26 10:40:46 +0200

    actually makes more sense to check first the Pub (?) I guess so

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -20,21 +20,22 @@ EC_KEY *ec_load(char const *folder)
 	if (!folder)
 		return (NULL);
 	/* concatene path */
-	sprintf(path, "%s/" PRI_FILENAME, folder);
+	sprintf(path, "%s/" PUB_FILENAME, folder);
 	fkey = fopen(path, "r");
-	if (!PEM_read_ECPrivateKey(fkey, &key_pair, NULL, NULL))
+	if (!PEM_read_EC_PUBKEY(fkey, &key_pair, NULL, NULL))
 	{
+		/* if there's even no pubkey */
+		EC_KEY_free(key_pair);
 		fclose(fkey);
 		return (NULL);
 	}
 	fclose(fkey);
 
 	/* concatene again, buffer will be flushed */
-	sprintf(path, "%s/" PUB_FILENAME, folder);
+	sprintf(path, "%s/" PRI_FILENAME, folder);
 	fkey = fopen(path, "r");
-	if (!PEM_read_EC_PUBKEY(fkey, &key_pair, NULL, NULL))
+	if (!PEM_read_ECPrivateKey(fkey, &key_pair, NULL, NULL))
 	{
-		EC_KEY_free(key_pair);
 		fclose(fkey);
 		return (NULL);
 	}
