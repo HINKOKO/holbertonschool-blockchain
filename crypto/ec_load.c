242f8a5e4b0144dc68be3101c90b2f729369e97a
HINKOKO <piotr62@live.fr>
2023-10-26 09:17:50 +0200

    try to 'set' the public key in EC_KEY struct

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -13,19 +13,28 @@
 
 EC_KEY *ec_load(char const *folder)
 {
+	/* construct the paths */
+	char priv_path[256], pub_path[256];
 	EC_KEY *eckey = EC_KEY_new();
-	FILE *fpriv = NULL, *fpub = NULL;
+	FILE *fpriv, *fpub;
+
+	snprintf(priv_path, sizeof(priv_path), "%s%s", folder, "key.pem");
+	snprintf(pub_path, sizeof(pub_path), "%s%s", folder, "key_pub.pem");
 
 	if (!folder || !eckey)
 		return (NULL);
 
-	fpriv = fopen(folder, "r");
+	fpriv = fopen(priv_path, "r");
 	eckey = PEM_read_ECPrivateKey(fpriv, &eckey, NULL, NULL);
 	fclose(fpriv);
 
-	fpub = fopen(folder, "r");
+	fpub = fopen(pub_path, "r");
 	eckey = PEM_read_EC_PUBKEY(fpub, &eckey, NULL, NULL);
 	fclose(fpub);
+
+	/* set Public Key in EC_KEy struct => stderr specified NULL */
+	/* provided to ec_to_pub() <= wasn't set ? */
+	EC_KEY_set_public_key(eckey, EC_KEY_get0_public_key(eckey));
 
 	EC_KEY_free(eckey);
 	return (eckey);
