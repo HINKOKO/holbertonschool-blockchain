0efc881a8101eda9a7d79828a272c3aba6490e08
HINKOKO <piotr62@live.fr>
2023-11-06 16:47:10 +0100

    destructing the blockchain, using llist_destroy lib functions

---
+++ blockchain/v0.1/blockchain_destroy.c
@@ -0,0 +1,17 @@
+#include "blockchain.h"
+
+/**
+ * blockchain_destroy - deletes an existing Blockchain,
+ * along with all the blocks it contains
+ * @blockchain: pointer to the Blockchain struct to wipe out
+ *
+ */
+
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+
+	llist_destroy(blockchain->chain, 1, NULL);
+	free(blockchain);
+}
