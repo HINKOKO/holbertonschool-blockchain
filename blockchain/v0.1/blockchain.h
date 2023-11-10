4bced0ed8be8db68512ef6f33ae486abe0222c79
HINKOKO <piotr62@live.fr>
2023-11-10 13:43:45 +0100

    updating header,embedding the genesis block pattern

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -22,6 +22,25 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define GENESIS                                                                \
+	{                                                                          \
+		{                                                                      \
+			/* info */                                                         \
+			0 /* index */,                                                     \
+			0,			/* difficulty */                                       \
+			1537578000, /* timestamp */                                        \
+			0,			/* nonce */                                            \
+			{0}			/* prev_hash */                                        \
+		},                                                                     \
+			{                                                                  \
+				/* data */                                                     \
+				"Holberton School", /* buffer */                               \
+				16					/* len */                                  \
+			},                                                                 \
+			"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+			"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+	}
+
 /**
  * struct blockchain_s - Blockchain structure
  * @chain: Linked linst of pointers to block_t
