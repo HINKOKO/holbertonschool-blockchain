5227512137e4d669e9165de0502815367e2928fe
HINKOKO <piotr62@live.fr>
2023-11-14 19:00:25 +0100

    fixing index + 1

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -13,12 +13,15 @@ uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 	block_t *last = NULL, *last_adjusted = NULL;
 
 	last = llist_get_tail(blockchain->chain);
-	last_adjusted = llist_get_node_at(blockchain->chain,
-									  (last->info.index - DIFFICULTY_ADJUSTMENT_INTERVAL));
 
 	if (!last || last->info.index == 0 ||
 		last->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL != 0)
 		return (last->info.difficulty);
+	/* at 'first' last adjusted is the last too */
+	/* when % DIFFCULTY => returns 0, the index is forcefully set to 5 */
+	/* add +1 to it avoid jumping back to 0 in the get_node_at(..idx 0) */
+	last_adjusted = llist_get_node_at(blockchain->chain,
+									  (last->info.index + 1 - DIFFICULTY_ADJUSTMENT_INTERVAL));
 
 	elapsed = last->info.timestamp - last_adjusted->info.timestamp;
 	expected = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
