da9b997fd1366929946aa877d95b24271b33deb9
HINKOKO <piotr62@live.fr>
2023-11-20 22:08:23 +0100

    What the hell

--- blockchain/v0.3/provided/_blockchain_print.c
+++ blockchain/v0.3/provided/_blockchain_print.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "blockchain.h"
+#include "transaction.h"
 
 void _print_hex_buffer(uint8_t const *buf, size_t len);
 int _transaction_print_loop(transaction_t const *transaction,
