fcb9942401185faa8d19eef74d3692732710ca73
HINKOKO <piotr62@live.fr>
2023-11-21 11:35:45 +0100

    Removing Invalid size read, by providing the correct size of digest L21

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -18,7 +18,7 @@ tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 
 	memcpy(inp->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
 	memcpy(inp->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
-	memcpy(inp->tx_out_hash, unspent->out.hash, sizeof(unspent->out));
+	memcpy(inp->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
 	memset(&(inp->sig), 0, 0);
 
 	return (inp);
