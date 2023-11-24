e0e1585a90b2b895ad2c2c10aeb51299d97c08df
HINKOKO <piotr62@live.fr>
2023-11-24 11:13:52 +0100

    Cleaner version of transaction creation, finally using llist_for_each to loop & perform action

--- blockchain/v0.3/transaction/transaction_create.c
+++ blockchain/v0.3/transaction/transaction_create.c
@@ -2,85 +2,86 @@
 
 #include <unistd.h>
 
-// if (!sign_input(tx, sender, all_unspent))
+/**
+ * sign_transaction - Sign tx inputs using tx->id passed in args
+ * @node: Node of LL of tx inputes
+ * @idx: index -> Unused
+ * @args: array of arguments
+ * Return: 0 Success, 1 if Failure
+ */
 
-int sign_input(transaction_t *tx, EC_KEY const *sender, llist_t *all_unspent)
+int sign_transaction(llist_node_t node, unsigned int idx, void *args)
 {
-	tx_in_t *txi;
-	uint32_t i = 0, len = llist_size(tx->inputs);
+	tx_in_t *tx_in = node;
+	void **ptr = args;
 
-	if (!transaction_hash(tx, tx->id))
-		return (0);
-	for (i = 0; i < len; i++)
-	{
-		txi = llist_get_node_at(tx->inputs, i);
-		if (!tx_in_sign(txi, txi->tx_id, sender, all_unspent))
-			return (0);
-	}
-	return (1);
-}
-
-transaction_t *create_utxos(uint32_t amount, uint8_t pub_s[EC_PUB_LEN],
-							uint8_t pub_r[EC_PUB_LEN], transaction_t *tx, uint32_t unspent)
-{
-	tx_out_t *txo_receiver = NULL, *txo_sender = NULL;
-
-	if (unspent > amount)
-	{
-		txo_sender = tx_out_create(unspent - amount, pub_s);
-	}
-	txo_receiver = tx_out_create(amount, pub_r);
-
-	if (llist_add_node(tx->outputs, txo_receiver, ADD_NODE_REAR) ||
-		(txo_sender && llist_add_node(tx->inputs, txo_sender, ADD_NODE_REAR)))
-		return (NULL);
-
-	return (tx);
-}
-
-int populate_input(llist_t *valids, uint32_t amount, uint8_t pub[EC_PUB_LEN], transaction_t *tx)
-{
-	unspent_tx_out_t *nodes = calloc(1, sizeof(*nodes));
-	int i, len = llist_size(valids);
-	tx_in_t *txi = NULL;
-
-	for (i = 0; i < len; i++)
-	{
-		nodes = llist_pop(valids);
-		txi = tx_in_create(nodes);
-		if (!txi)
-			return (0);
-		if (llist_add_node(tx->inputs, txi, ADD_NODE_REAR))
-			return (0);
-	}
-	return (1);
-	(void)amount;
+	if (!tx_in_sign(tx_in, ptr[0], ptr[1], ptr[2]))
+		return (1);
+	return (0);
+	(void)idx;
 }
 
 /**
- * collect_unspent - Collect from UTXO's the
- * @all_unspent: list of UTXOs
- * @pub_sender: contains pub key of sender
- * Return: uint32_t amount of available money from UTXOs the senser posess
+ * send_money - Send coins to receiver 'r', and eventually
+ * create a new utxo's for the leftover to the sender 's'
+ * @s: EC_KEY pair of sender
+ * @r: EC_KEY pair of receiver
+ * @amount: Amount being sent
+ * @total_money: Available money of the sender
+ * Return: Pointer to the txo which embed the leftover for the sender
+ * or NULL on failure
  */
 
-uint32_t collect_valid_utxo(llist_t *all_unspent, uint8_t pub_sender[EC_PUB_LEN], llist_t *valids)
+llist_t *send_money(EC_KEY const *s, EC_KEY const *r,
+					uint32_t amount, uint32_t total_money)
 {
-	unspent_tx_out_t *browser = NULL;
-	int i, unspent_len = llist_size(all_unspent);
-	uint32_t money = 0;
+	tx_out_t *tx_out;
+	llist_t *out; /* 'Shared' list of tx_out */
+	uint32_t leftover = total_money - amount;
+	uint8_t pub[EC_PUB_LEN];
 
-	for (i = 0; i < unspent_len; i++)
+	out = llist_create(MT_SUPPORT_FALSE);
+	ec_to_pub(r, pub);
+	tx_out = tx_out_create(amount, pub);
+	llist_add_node(out, tx_out, ADD_NODE_REAR);
+	if (leftover)
 	{
-		browser = llist_get_node_at(all_unspent, i);
-		if (memcmp(browser->out.pub, pub_sender, EC_PUB_LEN) == 0)
-		{
-			money += browser->out.amount;
-			llist_add_node(valids, browser, ADD_NODE_REAR);
-		}
+		ec_to_pub(s, pub);
+		tx_out = tx_out_create(leftover, pub);
+		llist_add_node(out, tx_out, ADD_NODE_REAR);
 	}
+	return (out);
+}
 
-	return (money);
+/**
+ * collect_unspent - The actual 'action' being performerd on each node
+ * of the all_unspent list -> Collect valids UTXO's of the sender
+ * @node: Custom node from opaque llist_node_t struct, here
+ * we are 'casting' it to an `unspent_tx_out` "node" (what we need actually)
+ * @idx: Index which iterates on the `llist_for_each` invocation (unused)
+ * @args: Pointer to arguments which were passed to the `llist_for_each`
+ * -> To pass additional info to this function, very neat.
+ * Return: Always SUCCESS
+ */
+
+int collect_unspent(llist_node_t node, unsigned int idx, void *args)
+{
+	unspent_tx_out_t *browser = node;
+	/* args interpreted as pointer to an array of void pointers */
+	/* Common tech in C to pass mult pieces of data through single pointer */
+	void **ptr = args;
+	llist_t *inputs = ptr[1];
+	uint32_t *money = ptr[2];
+	tx_in_t *tx_in; /* What we want to create as we collect */
+
+	if (!memcmp(browser->out.pub, ptr[0], EC_PUB_LEN))
+	{
+		tx_in = tx_in_create(browser);
+		llist_add_node(inputs, tx_in, ADD_NODE_REAR);
+		*money += browser->out.amount;
+	}
+	return (EXIT_SUCCESS);
+	(void)idx;
 }
 
 /**
@@ -93,49 +94,47 @@ uint32_t collect_valid_utxo(llist_t *all_unspent, uint8_t pub_sender[EC_PUB_LEN]
  * Return: Pointer to the created transaction, NULL on failure
  * Steps ==>
  * -- Select a set of unspent tx outputs from `all_unspent`, which
- * pub_key match `sender` privatekey
- * -- Create tx inputs from these selected UTXO's
+ * pub_key match `sender` privatekey => `collect_unspent()`
+ * -- Create tx inputs from these selected UTXO's => `collect_unspent()`
  * -- Function must FAILS if sender doesn't possess enough coins
- * (UTXO's < amount)
+ * (UTXO's < amount) => Checked in `transaction_create()`
  * -- Create a tx output, sending `amount` coins to `receiver's` pubkey
  * -- If total UTXO's amount > amount => Create 2nd tx output,
- * for sending leftover back to `sender`
+ * for sending leftover back to `sender` => Performed in `send_money()`
  * -- Compute tx ID (hash)
- * -- Sign tx inputs using previously computed tx ID
+ * -- Sign tx inputs using previously computed tx ID => We hash ID of tx in
+ * `transaction_create`, iterate on txinputs to sign 'em in `sign_transaction`
  */
 
 transaction_t *transaction_create(EC_KEY const *sender, EC_KEY const *receiver,
 								  uint32_t amount, llist_t *all_unspent)
 {
 	transaction_t *tx;
-	llist_t *valids = llist_create(MT_SUPPORT_TRUE);
-	uint8_t pub_sender[EC_PUB_LEN], pub_receiver[EC_PUB_LEN];
-	uint32_t sender_money = 0;
-
-	/* Check that everythin is provided for tx */
-	if (!sender || !receiver || !amount || !all_unspent)
-		return (NULL);
-	if (!ec_to_pub(sender, pub_sender) || !ec_to_pub(receiver, pub_receiver))
-		return (fprintf(stderr, "Failed to read sender pub key"), NULL);
-
-	/* Collect UTXOs that matches and computes money available */
-	sender_money = collect_valid_utxo(all_unspent, pub_sender, valids);
-	if (sender_money < amount)
-		return (fprintf(stderr, "Not enough money, get back to work"), NULL);
+	uint8_t pub[EC_PUB_LEN]; /* 'Buffer' to extract pub keys of both parties */
+	void *args[3] = {0};	 /* Array to hold 3 void pointers */
+	llist_t *tx_out, *tx_in = llist_create(MT_SUPPORT_FALSE);
+	uint32_t total_money = 0;
 
 	tx = calloc(1, sizeof(*tx));
-	tx->inputs = llist_create(MT_SUPPORT_FALSE);
-	tx->outputs = llist_create(MT_SUPPORT_FALSE);
-
-	if (!populate_input(valids, amount, pub_sender, tx))
+	if (!tx || !sender || !receiver || !amount || !all_unspent)
 		return (NULL);
+	if (!ec_to_pub(sender, pub))
+		return (fprintf(stderr, "Error: Failed to extract sender pub key"),
+				NULL);
+	/* Pick the args we"re gonna need for this operation of collect */
+	args[0] = pub, args[1] = tx_in, args[2] = &total_money;
+	llist_for_each(all_unspent, collect_unspent, args);
 
-	tx = create_utxos(amount, pub_sender, pub_receiver, tx, sender_money);
-	if (!tx)
+	if (total_money < amount)
+	{
+		free(tx);
 		return (NULL);
-
-	if (!sign_input(tx, sender, all_unspent))
-		return (free(tx), NULL);
-
+	}
+	tx_out = send_money(sender, receiver, amount, total_money);
+	tx->inputs = tx_in, tx->outputs = tx_out;
+	transaction_hash(tx, tx->id);
+	/* Assign new arguments for signing */
+	args[0] = tx->id, args[1] = (void *)sender, args[2] = all_unspent;
+	llist_for_each(tx->inputs, sign_transaction, args);
 	return (tx);
 }
