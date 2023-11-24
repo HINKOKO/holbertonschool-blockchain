e677d0e29f2c42aea284a1aa4bb185d0f4133ada
HINKOKO <piotr62@live.fr>
2023-11-24 11:18:21 +0100

    Update header file to the end

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -90,5 +90,11 @@ uint8_t *transaction_hash(transaction_t const *transaction,
 sig_t *tx_in_sign(tx_in_t *in,
 				  uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 				  EC_KEY const *sender, llist_t *all_unspent);
+transaction_t *transaction_create(EC_KEY const *sender, EC_KEY const *receiver,
+								  uint32_t amount, llist_t *all_unspent);
+int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent);
+transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index);
+int coinbase_is_valid(transaction_t const *coinbase, uint32_t block_index);
+void transaction_destroy(transaction_t *transaction);
 
 #endif /* __TX__ */
