3138e5035df5d4da2138713a7e0bd1d12f1d84bc
HINKOKO <piotr62@live.fr>
2023-11-26 17:13:21 +0100

    Destroy list of transactions with

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -13,6 +13,6 @@ void block_destroy(block_t *block)
 	if (!block)
 		return;
 
-	llist_destroy(block->transactions, 1, NULL);
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
 	free(block);
 }
