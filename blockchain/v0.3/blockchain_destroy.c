37612152e619091ee7b82f2b91524ef894d09071
HINKOKO <piotr62@live.fr>
2023-11-26 17:25:27 +0100

    inp & out aren't in unspent, calling block_destroy on blockchain->chain , which points to the block_t dude

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -12,7 +12,7 @@ void blockchain_destroy(blockchain_t *blockchain)
 	if (!blockchain)
 		return;
 
-	llist_destroy(blockchain->unspent, 1, (node_dtor_t)block_destroy);
-	llist_destroy(blockchain->chain, 1, NULL);
+	llist_destroy(blockchain->chain, 1, (node_dtor_t)block_destroy);
+	llist_destroy(blockchain->unspent, 1, free);
 	free(blockchain);
 }
