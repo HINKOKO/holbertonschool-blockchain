e66476a1b8ac789d3032bfb54d546963c83acfdf
HINKOKO <piotr62@live.fr>
2023-11-27 14:56:48 +0100

    What the heck , test sandbox via Checker

--- blockchain/v0.3/block_is_valid.c
+++ blockchain/v0.3/block_is_valid.c
@@ -1,27 +1,30 @@
 #include "blockchain.h"
 
-#define CLEAR_HASH(h) (memset(h, 0, sizeof(h)))
+int check_tx(llist_node_t node, unsigned int idx, void *unspent)
+{
+	if (idx == 0)
+		return (0);
+	if (!transaction_is_valid(node, unspent))
+		return (1);
+	return (0);
+}
 
 /**
  * block_is_valid - Verifies that a Block is
  * => Not Invalid === Is valid hahaha!
  * @block: Pointer to the block to check
  * @prev_block: Pointer to previous Block
- * (NULL) if we stand on Genesis
+ * @all_unspent: Pointer to all utxos
+ *
+ * -- Updates --
+ * A block must contain at least one transaction (at least coinbase tx)
+ * The first transaction in a block must be a valid coinbase tx
  *
  * Return: EXIT_SUCCESS||FAILURE
- *
- * -> `block` shouldn't be NULL
- * -> prev_block can be NULL if only if block's index is 0
- * -> prev_block index must be current + 1
- * -> computed hash of prev_block must match the one it stores
- * -> computed hash of prev_block must match the one stored in 'block'
- * -> computed hash of 'block' must match the one it stores
- * -> block->data.len must not exceed BLOCKCHAIN_DATA_MAX
- * => we skip timestamp and difficulty for this iteration of the project
  */
 
-int block_is_valid(block_t const *block, block_t const *prev_block)
+int block_is_valid(block_t const *block, block_t const *prev_block,
+				   llist_t *all_unspent)
 {
 	uint8_t hash_comparator[SHA256_DIGEST_LENGTH] = {0};
 	block_t const gen_copy = GENESIS;
@@ -35,21 +38,26 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 	if (block->info.index != prev_block->info.index + 1)
 		return (1);
 	if (!block_hash(prev_block, hash_comparator) ||
-		memcmp(hash_comparator, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
-		return (1);
-	if (memcmp(prev_block->hash, block->info.prev_hash,
-			   SHA256_DIGEST_LENGTH) != 0)
-		return (1);
-	CLEAR_HASH(hash_comparator);
+		memcmp(hash_comparator, prev_block->hash, SHA256_DIGEST_LENGTH) ||
+		memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH) ||
+		!block_hash(block, hash_comparator))
 
-	if (!block_hash(block, hash_comparator) ||
-		memcmp(hash_comparator, block->hash, SHA256_DIGEST_LENGTH) != 0)
-		return (1);
+		if (!block_hash(block, hash_comparator) ||
+			memcmp(hash_comparator, block->hash, SHA256_DIGEST_LENGTH) != 0)
+			return (1);
 	/* Using our new match_difficulty functionality to extend validation */
 	if (!hash_matches_difficulty(block->hash, block->info.difficulty))
 		return (1);
 
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
 		return (1);
+	/* Updated additional checks */
+	if (llist_size(block->transactions) < 1)
+		return (1);
+	if (!coinbase_is_valid(llist_get_head(block->transactions),
+						   block->info.index))
+		return (1);
+
 	return (0);
+	(void)all_unspent; /* DIsabled for the moment */
 }
--- blockchain/v0.3/test/block_is_valid-main.c
+++ blockchain/v0.3/test/block_is_valid-main.c
@@ -1,34 +1,61 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "../blockchain.h"
+#include "blockchain.h"
+
+void _blockchain_print_brief(blockchain_t const *blockchain);
+void _print_hex_buffer(uint8_t const *buf, size_t len);
+
+static block_t *_add_block(blockchain_t *blockchain, block_t const *prev,
+                           char const *data, EC_KEY *miner)
+{
+    block_t *block;
+    transaction_t *coinbase;
+
+    block = block_create(prev, (int8_t *)data, (uint32_t)strlen(data));
+    block->info.difficulty = 20;
+
+    coinbase = coinbase_create(miner, block->info.index);
+    llist_add_node(block->transactions, coinbase, ADD_NODE_FRONT);
+
+    block_mine(block);
+
+    if (block_is_valid(block, prev, blockchain->unspent) == 0)
+    {
+        printf("Block mined: [%u] ", block->info.difficulty);
+        _print_hex_buffer(block->hash, SHA256_DIGEST_LENGTH);
+        printf("\n");
+        llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+    }
+    else
+    {
+        fprintf(stderr, "Invalid Block with index: %u\n",
+                block->info.index);
+    }
+
+    return (block);
+}
 
 /**
  * main - Entry point
  *
- * Return: EXIT_SUCCESS||FAILURE
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
-
 int main(void)
 {
-	blockchain_t *blockchain;
-	block_t *first, *block;
+    blockchain_t *blockchain;
+    block_t *block;
+    EC_KEY *miner;
 
-	blockchain = blockchain_create();
-	first = llist_get_head(blockchain->chain);
+    miner = ec_create();
+    blockchain = blockchain_create();
 
-	block = block_create(first, (int8_t *)"Holberton", 9);
-	block_hash(block, block->hash);
-	llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+    block = llist_get_head(blockchain->chain);
+    block = _add_block(blockchain, block, "Holberton", miner);
+    block = _add_block(blockchain, block, "School", miner);
 
-	if (block_is_valid(first, NULL) != 0 ||
-		block_is_valid(block, first) != 0)
-	{
-		fprintf(stderr, "Invalid Block\n");
-		return (EXIT_FAILURE);
-	}
-	printf("Blockchain is Valid dude!\n");
-	blockchain_destroy(blockchain);
-
-	return (EXIT_SUCCESS);
+    blockchain_destroy(blockchain);
+    EC_KEY_free(miner);
+    return (EXIT_SUCCESS);
 }
